85920d4ae23b2267354c4e772e0499bee78b3eb1
Lavorary <hei.lavorary@gmail.com>
2026-08-03 00:58:36 +0300

    feat : create Jmodel for seat

---
+++ src/main/java/com/hei/school/repository/model/JSeat.java
@@ -0,0 +1,23 @@
+package com.hei.school.repository.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="seats")
+public class JSeat {
+    @Id @UuidGenerator
+    private String id;
+
+    private String number;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="room_id")
+    private JRoom room;
+}
