e3ed997b92398115d719d5458863300d803ccac9
Lavorary <hei.lavorary@gmail.com>
2026-08-03 00:07:18 +0300

    feat : create Projection class

---
+++ src/main/java/com/hei/school/model/Projection.java
@@ -0,0 +1,20 @@
+package com.hei.school.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Projection {
+    private UUID id;
+    private Instant datetime;
+    private BigDecimal seatPrice;
+}
