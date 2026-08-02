5f24934a1798234745019fe09bc9b61f30739504
Lavorary <hei.lavorary@gmail.com>
2026-08-03 00:04:05 +0300

    fix : remove useless import in User.java

--- src/main/java/com/hei/school/model/User.java
+++ src/main/java/com/hei/school/model/User.java
@@ -1,9 +1,7 @@
 package com.hei.school.model;
 
 import com.hei.school.enums.UserRole;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.UUID;
