71445a7442f0cf226d8bc0f10073f6c8394beeee
Lavorary <hei.lavorary@gmail.com>
2026-08-03 00:58:26 +0300

    feat : create Jmodel for room

---
+++ src/main/java/com/hei/school/repository/model/JRoom.java
@@ -0,0 +1,23 @@
+package com.hei.school.repository.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="rooms")
+public class JRoom {
+    @Id @UuidGenerator
+    private String id;
+
+    private String number;
+
+    private Integer capacity;
+}
