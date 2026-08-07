ca975d44bf3d6328bde37682db93fc57328fc283
Lavorary <hei.lavorary@gmail.com>
2026-08-07 08:19:49 +0300

    feat : add mapper for Room and Seat

---
+++ src/main/java/com/hei/school/repository/mapper/RoomMapper.java
@@ -0,0 +1,34 @@
+package com.hei.school.repository.mapper;
+
+import com.hei.school.model.Room;
+import com.hei.school.repository.model.JRoom;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+public class RoomMapper {
+    public Room toModel(JRoom jroom) {
+        if(jroom == null) return null;
+
+        Room room = new Room();
+        room.setId(jroom.getId());
+        room.setNumber(jroom.getNumber());
+        room.setCapacity(jroom.getCapacity());
+        return room;
+    }
+
+    public JRoom toEntity(Room room) {
+        if(room == null) return null;
+        JRoom jroom = new JRoom();
+        jroom.setId(room.getId());
+        jroom.setNumber(room.getNumber());
+        jroom.setCapacity(room.getCapacity());
+        return jroom;
+
+    }
+}
---
+++ src/main/java/com/hei/school/repository/mapper/SeatMapper.java
@@ -0,0 +1,35 @@
+package com.hei.school.repository.mapper;
+
+import com.hei.school.model.Room;
+import com.hei.school.model.Seat;
+import com.hei.school.repository.model.JSeat;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+public class SeatMapper {
+    public Seat toModel(JSeat jseat) {
+        if(jseat == null) return null;
+
+        Seat seat = new Seat();
+        seat.setId(UUID.fromString(jseat.getId()));
+        seat.setNumber(jseat.getNumber());
+        seat.setRoom((Room) jseat.getRoom());
+        return seat;
+    }
+
+    public JSeat toEntity(Seat seat) {
+        if(seat == null) return null;
+
+
+        JSeat jSeat = new JSeat();
+        jSeat.setId(String.valueOf(seat.getId()));
+        jSeat.setNumber(seat.getNumber());
+        jSeat.setRoom(seat.getRoom());
+        return jSeat;
+
+    }
+}
