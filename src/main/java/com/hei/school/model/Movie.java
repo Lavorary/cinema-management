de88c12c1a786d7a2e498a6c8303ce0dc4e6ee08
Diarilala <hei.diarilala@gmail.com>
2026-08-08 18:44:05 +0300

    fix: change class movie to record

--- src/main/java/com/hei/school/model/Movie.java
+++ src/main/java/com/hei/school/model/Movie.java
@@ -3,21 +3,15 @@ package com.hei.school.model;
 import com.hei.school.enums.Genre;
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-public class Movie {
-  private UUID id;
-  private String title;
-  private List<Genre> genres;
-  private String description;
-  private Duration duration;
-  private List<Projection> projection;
-}
+public record Movie (
+  UUID id,
+  String title,
+  List<Genre> genres,
+  String description,
+  Duration duration,
+  Set<Projection> projections) {}
