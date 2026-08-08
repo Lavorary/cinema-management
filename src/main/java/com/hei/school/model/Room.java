b35c80a7b8aa64887678e1032e9a215bd15df8e6
Diarilala <hei.diarilala@gmail.com>
2026-08-08 18:46:38 +0300

    fix: change room class to record

--- src/main/java/com/hei/school/model/Room.java
+++ src/main/java/com/hei/school/model/Room.java
@@ -2,19 +2,12 @@ package com.hei.school.model;
 
 import java.util.List;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Builder
-public class Room {
-  private UUID id;
-  private String number;
-  private int capacity;
-  private List<Seat> seats;
-  private List<Projection> projections;
-}
+public record Room(
+  UUID id,
+  String number,
+  int capacity,
+  List<Seat> seats) {}
