a3b5b87b9414e1692122cd937461e949636cdac0
Diarilala <hei.diarilala@gmail.com>
2026-08-08 18:47:15 +0300

    fix: change id type from String to UUID feat: add bidirectional relationship of seat and projection

--- src/main/java/com/hei/school/repository/model/JRoom.java
+++ src/main/java/com/hei/school/repository/model/JRoom.java
@@ -1,22 +1,35 @@
 package com.hei.school.repository.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.UuidGenerator;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
 
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Getter
+@Setter
 @Table(name = "rooms")
 public class JRoom {
-  @Id @UuidGenerator private String id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
   private String number;
 
   private Integer capacity;
+
+  @OneToMany(mappedBy = "room_id", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<JSeat> seats;
+
+  @OneToMany(mappedBy = "room_id", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<JProjection> projections;
 }
