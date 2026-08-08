4bb39583f07d454aa7505f7f02171aa4b7901a45
Diarilala <hei.diarilala@gmail.com>
2026-08-08 18:39:17 +0300

    fix: change Seat class to record without room attribut

--- src/main/java/com/hei/school/model/Seat.java
+++ src/main/java/com/hei/school/model/Seat.java
@@ -1,17 +1,9 @@
 package com.hei.school.model;
 
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class Seat {
-  private UUID id;
-  private String number;
-  private Room room;
-}
+public record Seat(
+  UUID id,
+  String number) {}
