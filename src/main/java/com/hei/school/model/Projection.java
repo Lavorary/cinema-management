c6457f51fb410f5fd1464a3ed7f4082dcccf443f
Diarilala <hei.diarilala@gmail.com>
2026-08-08 18:42:36 +0300

    fix: change projection class to record

--- src/main/java/com/hei/school/model/Projection.java
+++ src/main/java/com/hei/school/model/Projection.java
@@ -2,22 +2,13 @@ package com.hei.school.model;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.List;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
+
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class Projection {
-  private UUID id;
-  private Instant datetime;
-  private BigDecimal seatPrice;
-  private Movie movie;
-  private Room room;
-  private List<Reservation> reservations;
-}
+public record Projection (
+  UUID id,
+  Instant datetime,
+  BigDecimal seatPrice,
+  Room room) {}
